b819586d240182666e5e5441b7e02a5bcf8fee0a
Yin <doyin@deloitte.com.cn>
2024-03-27 17:45:47 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,10 @@
+const getMaxValue = require('./getMaxValue')
+const print = require('./print')
+
+module.exports = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
